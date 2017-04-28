97b1a937e71dfd372e48c1ba70200edca4d15720
Vincent Guarnaccia <Vincent.Guarnaccia@gmail.com>
2017-04-27 22:00:24 -0700

    fresh install successful

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 echo 'set runtimepath+=~/.vim_config
 
-source ~/.vim/default.vim
-source ~/.vim/plugins_config.vim
+source ~/.vim_config/default.vim
+source ~/.vim_config/plugins_config.vim
 
 try
-source ~/.vim/experimental.vim
+source ~/.vim_config/experimental.vim
 catch
 endtry' > ~/.vimrc
 
